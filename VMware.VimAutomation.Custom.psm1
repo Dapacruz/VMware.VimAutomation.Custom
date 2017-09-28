8f42b0f65738fe7fbc17bd22609dcb6f0adf371c
David Cruz <david@thecruzs.net>
2017-09-27 18:19:28 -0700

    Add Get-VMHostVirtualToPhysicalCpuRatio cmdlet.

--- VMware.VimAutomation.Custom.psm1
+++ VMware.VimAutomation.Custom.psm1
@@ -662,20 +662,21 @@ function Import-VMHostNetworkingFromCsv {
 function Test-VMHostNetworking {
     [CmdletBinding()]
     Param (
-        [Parameter(ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, Mandatory=$true, Position=0)][Alias('Name')]
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Mandatory, Position=0)][Alias('Name')]
         [string[]]$VMHosts,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory)]
         [string[]]$VMkernelPort,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory)]
         [string[]]$IpAddress,
         [int]$Mtu = 1472
     )
     Begin {
-        # Expand to full hostname in case wildcards are used
-        $VMHosts = Get-VMHost -Name $VMHosts
         $failures = 0
     }
     Process {
+        # Expand to full hostname in case wildcards are used
+        $VMHosts = Get-VMHost -Name $VMHosts
+        
         foreach ($VMHost in $VMHosts) {
             $esxcli = Get-EsxCli -VMHost $vmhost -V2
             $ping = $esxcli.network.diag.ping
@@ -706,5 +707,68 @@ function Test-VMHostNetworking {
         } else {
             Write-Host "Ping sweep complete. Failures detected: $failures."
         }
+    }
+}
+
+
+<#
+        .Synopsis
+        Calculate the virtual to physcial CPU ratio
+        .Description
+        Calculate the virtual to physical CPU ratio for VMHosts provided
+        .Parameter VMHost
+        The VMHost you want to calculate the virtual to physical CPU ratio of. Can be a single host or multiple hosts provided by the pipeline. Wildcards are supported
+        .Example
+        PS C:\>Get-VMHostVirtualToPhysicalCpuRatio -VMHost esxi*
+
+        Calculate the virtual to physical CPU ratio of all ESXi hosts with names that begin with 'esxi'
+        .Link
+        https://github.com/Dapacruz/VMware.VimAutomation.Custom
+#>
+function Get-VMHostVirtualToPhysicalCpuRatio {
+    [CmdletBinding()]
+    Param (
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName, Mandatory, Position=0)][Alias('Name', 'VMHosts')]
+        [string[]]$VMHost
+    )
+    Begin {
+        $results = @()
+    }
+    Process {
+        # Expand to full hostname in case wildcards are used
+        $esxi_host = Get-VMHost -Name $VMHost
+
+        foreach ($h in $esxi_host) {
+            $vcpu_count = 0
+            $ratio = 0
+            $pcpu_core_count = $h.ExtensionData.Hardware.CpuInfo.NumCpuCores
+            $pcpu_thread_count = $h.ExtensionData.Hardware.CpuInfo.NumCpuThreads
+            $virtual_machines = Get-VM -Location $h
+            
+            foreach ($vm in $virtual_machines) {
+                $vcpu_count += $vm.NumCpu
+            }
+            
+            if ($vcpu_count -ne 0) {
+                $physical_ratio = $vcpu_count/$pcpu_core_count
+                $logical_ratio = $vcpu_count/$pcpu_thread_count
+             } else {
+                $physical_ratio = 0
+                $logical_ratio = 0
+            }
+            
+            $obj = New-Object PSObject
+            $obj | Add-Member -MemberType NoteProperty -Name 'Name' -Value $h.Name
+            $obj | Add-Member -MemberType NoteProperty -Name 'vCPUs' -Value $vcpu_count
+            $obj | Add-Member -MemberType NoteProperty -Name 'PhysicalCores' -Value $pcpu_core_count
+            $obj | Add-Member -MemberType NoteProperty -Name 'PhysicalRatio' -Value $('{0:N3}:1' -f $physical_ratio)
+            $obj | Add-Member -MemberType NoteProperty -Name 'LogicalCores' -Value $pcpu_thread_count
+            $obj | Add-Member -MemberType NoteProperty -Name 'LogicalRatio' -Value $('{0:N3}:1' -f $logical_ratio)
+                                    
+            $results += $obj
+        }
+    }
+    End {
+        Write-Output $results
     }
 }
